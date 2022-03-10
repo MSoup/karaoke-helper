e046ec26f82e00d29beccacb859466fb63042767
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-10 00:58:42 -0500

    add categories to tiles

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -2,18 +2,18 @@ import Tile from '../components/Tile'
 import './Main.css';
 
 function Main() {
-  const searchBar = <div id="search-bar">Search</div>;
+  const searchBar = <div id="search-bar">Search by Keyword</div>;
+  const searchCategories = ['Artist','Song Title','Album','Genre','History'];
+
+  const categoryTiles = searchCategories.map(category => <Tile title={category} />);
 
   return (
     <div className="Main">
       {/* Search Bar */}
       {searchBar}
-      <Tile title={'Title'} />
-      <Tile title={'Title'} />
-      <Tile title={'Title'} />
-      <Tile title={'Title'} />
-      <Tile title={'Title'} />
-      <Tile title={'Title'} />
+
+      {/* Tiles */}
+      {categoryTiles}
     </div>
   );
 }
