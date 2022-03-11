aae6552e2d0ed40176215935284b1263bc79ec37
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-11 17:21:46 -0500

    implement search bar

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,32 +1,32 @@
-import Tile from '../components/Tile'
-import SearchBar from '../components/SearchBar'
 import './Main.css';
-import Results from '../components/Results';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Tile from '../components/Tile'
+import SearchBar from '../components/SearchBar'
+import Results from '../components/Results';
 
 function Main() {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
   
   const limit = 10;
+  const options = {
+    params: { limit: 10, offset: 0 },
+    headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + process.env.BEARER_TOKEN,
+        'Content-Type': 'application/json'
+    }
+  };
   useEffect(() => {
-    const options = {
-      params: { limit: 10, offset: 0 },
-      headers: {
-          Accept: 'application/json',
-          Authorization: 'Bearer ' + process.env.BEARER_TOKEN,
-          'Content-Type': 'application/json'
-      }
-    };
     const url = `https://api.spotify.com/v1/search?q=${term}&type=track&limit=${limit}`;
     axios["default"].get(url, options)
-        .then(data => {
-        const results =  data.data.tracks.items;
-        setResults(results);
-        console.log(results);
+      .then(data => {
+      const results =  data.data.tracks.items;
+      setResults(results);
+      console.log('results ----> ',results);
     })["catch"](function (err) {
-        console.log(err.response);
+      console.log(err.response);
     });
   }, [term]);
 
@@ -40,7 +40,7 @@ function Main() {
       />);
   return (
     <div className="Main">
-      {/* <SearchBar onSearch={term => setTerm(term)} /> */}
+      <SearchBar onSearch={term => setTerm(term)} />
       {/* <SearchBar /> */}
       <Results results={results} />
       {/* Tiles */}
