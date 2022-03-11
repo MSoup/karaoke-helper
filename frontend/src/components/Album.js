a6d16cd59b9208165aaaf5699fc3f86583406af0
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-11 14:59:13 -0500

    fix album component

--- frontend/src/components/Album.js
+++ frontend/src/components/Album.js
@@ -4,7 +4,12 @@ export default function Album(props) {
   // const albumInfoClass = classnames("album__info", {
   //   "album__info--explicit": props.collectionExplicitness === "explicit"
   // });
-  const { albumImages, albumName, artistName, isExplicit } = props;
+  // const { albumImages, albumName, artistName, isExplicit } = props;
+  // const { albumImages, albumName, artistName, isExplicit } = searchResults;
+  const albumImages = props.album.images;
+  const albumName = props.album.name;
+  const artistName = props.album.artists;
+  const isExplicit = props.explicit;
 
   return (
     <article className="album">
