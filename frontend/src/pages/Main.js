da650d051b1dedac6cf06be6c21b30bca9a6ae0d
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-11 17:56:35 -0500

    fix bearer token

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -15,7 +15,7 @@ function Main() {
     params: { limit: 10, offset: 0 },
     headers: {
         Accept: 'application/json',
-        Authorization: 'Bearer ' + '',
+        Authorization: 'Bearer ' + process.env.BEARER_TOKEN,
         'Content-Type': 'application/json'
     }
   };
